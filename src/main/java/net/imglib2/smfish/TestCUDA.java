b402cf556788d4f3170cdc6862583e69167c8a4e
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-07-25 17:38:45 -0400

    opening a file

--- src/main/java/net/imglib2/smfish/TestCUDA.java
+++ src/main/java/net/imglib2/smfish/TestCUDA.java
@@ -1,10 +1,16 @@
 package net.imglib2.smfish;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.real.FloatType;
 import spim.process.cuda.CUDASeparableConvolution;
 import spim.process.cuda.CUDATools;
 import spim.process.cuda.NativeLibraryTools;
+import spim.process.fusion.export.DisplayImage;
 
 /**
  * 
@@ -18,6 +24,13 @@ public class TestCUDA
 		System.out.println( cuda.multipleOfX_31() );
 		System.out.println( cuda.multipleOfY_31() );
 		System.out.println( cuda.multipleOfZ_31() );
+		
+		final File f = new File( "img_1280x1024x128.tif" );
+		System.out.println( f.getAbsolutePath() );
+		
+		final Img< FloatType > img = OpenImg.open( f.getAbsolutePath(), new ArrayImgFactory<FloatType>() );
+		//ImageJFunctions.show( img );
+		new DisplayImage().exportImage(img, f.getAbsolutePath() );
 	}
 	
 	public static void main( String[] args )
