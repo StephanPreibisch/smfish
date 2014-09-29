c36cd1f0e1a1593a9e62837b998d2f811265508d
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-09-29 19:05:18 -0400

    added code to interactively deal with the segmentation

---
+++ src/main/java/net/imglib2/analyzesegmentation/Cell.java
@@ -0,0 +1,96 @@
+package net.imglib2.analyzesegmentation;
+
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.util.Util;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.sun.j3d.utils.geometry.Sphere;
+
+class Cell
+{
+	private final int id;
+
+	private int radius;
+
+	private final RealPoint position;
+
+	private Sphere sphere;
+
+	public Cell( final int id, final RealLocalizable position, final int radius )
+	{
+		this.id = id;
+		this.position = new RealPoint( position );
+		this.radius = radius;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "cell( " + id + ", " + Util.printCoordinates( position ) + ", " + radius + " )";
+	}
+
+	public void setSphere( final Sphere sphere ) { this.sphere = sphere; }
+	public Sphere getSphere() { return sphere; }
+
+	public int getRadius()
+	{
+		return radius;
+	}
+
+	public void setRadius( final int radius )
+	{
+		this.radius = radius;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public RealPoint getPosition()
+	{
+		return position;
+	}
+
+	public static Cell fromXml( final Element cell )
+	{
+		final int id = Integer.parseInt( cell.getElementsByTagName( "id" ).item( 0 ).getTextContent() );
+		final String data = ( ( Element ) cell.getElementsByTagName( "position" ).item( 0 ) ).getAttribute( "data" );
+		final String[] fields = data.split( "\\s+" );
+		final int n = fields.length;
+		final RealPoint position = new RealPoint( n );
+		for ( int d = 0; d < n; ++d )
+			position.setPosition( Double.parseDouble( fields[ d ] ), d );
+		final int radius = Integer.parseInt( cell.getElementsByTagName( "radius" ).item( 0 ).getTextContent() );
+		return new Cell( id, position, radius );
+	}
+
+	public static Element toXml( final Document doc, final Cell cell )
+	{
+		final Element elem = doc.createElement( "sphere" );
+
+		elem.appendChild( intElement( doc, "id", cell.getId() ) );
+		final RealLocalizable pos = cell.getPosition();
+		final int n = pos.numDimensions();
+		String data = "";
+		for ( int d = 0; d < n; ++d )
+			data += pos.getDoublePosition( d ) + ( ( d == n - 1 ) ? "" : " " );
+		final Element poselem = doc.createElement( "position" );
+		poselem.setAttribute( "data", data );
+		elem.appendChild( poselem );
+		elem.appendChild( intElement( doc, "radius", cell.getRadius() ) );
+
+		return elem;
+	}
+
+	public static Element intElement( final Document doc, final String name, final int value )
+	{
+		final Element e = doc.createElement( name );
+		e.appendChild( doc.createTextNode( Integer.toString( value ) ) );
+		return e;
+	}
+
+}
---
+++ src/main/java/net/imglib2/analyzesegmentation/VisualizeSegmentation.java
@@ -0,0 +1,301 @@
+package net.imglib2.analyzesegmentation;
+
+import ij3d.Content;
+import ij3d.Image3DUniverse;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.media.j3d.TransparencyAttributes;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import mpicbg.imglib.multithreading.SimpleMultiThreading;
+import mpicbg.spim.registration.bead.Bead;
+import mpicbg.spim.vis3d.VisualizeBeads;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.sun.j3d.utils.geometry.Sphere;
+
+import customnode.CustomPointMesh;
+import customnode.Mesh_Maker;
+
+public class VisualizeSegmentation
+{
+	public static String defaultXML = "/Users/preibischs/Documents/Microscopy/smFISH/samidouble_43_reconstructed.cells.xml";
+
+	final JFileChooser fileChooser;
+	final HashMap< Integer, Cell > cells = new HashMap< Integer, Cell >();
+	int nextCellId = 0;
+
+	public VisualizeSegmentation()
+	{
+		this.fileChooser = createFileChooser();
+
+		if ( this.loadAnnotations() )
+		{
+			final Image3DUniverse univ = VisualizeBeads.initUniverse();
+
+			final ArrayList< Bead > beads = new ArrayList<Bead>();
+			for ( int i = 0; i < 100; ++i )
+				beads.add( new Bead( i, new float[]{ i*10, i*10, i*10 }, null ) );
+
+			drawInvisibleBoundingBox( univ, cells );
+			drawCells( univ, cells, new Transform3D(), new Color3f( 1, 0, 1 ), 0.15f );
+
+			//VisualizationFunctions.drawArrow( univ, new Vector3f( new float[]{ 100, 100, 100 } ), 45, 10 );
+			//drawNuclei( univ, cells, new Transform3D(), 0.95f );
+		}
+	}
+
+	public void discoWorm()
+	{
+		Random random = new Random();
+		
+		while ( System.currentTimeMillis() > 0 )
+		{
+			for ( final Cell cell : cells.values() )
+			{
+				final int r = random.nextInt( 256 );
+				final int g = random.nextInt( 256 );
+				final int b = random.nextInt( 256 );
+				cell.getSphere().getAppearance().getColoringAttributes().setColor( new Color3f( r/255f, g/255f, b/255f ) );
+			}
+			
+			SimpleMultiThreading.threadWait( 100 );
+		}
+	}
+
+	public static BranchGroup drawCells( final Image3DUniverse univ, final Map< Integer, Cell > cells, final Transform3D globalTransform, 
+			final Color3f color, final float transparency )
+	{		
+		// get the scene
+		final BranchGroup parent = univ.getScene();
+
+		// create a new branch group that contains all transform groups which contain one sphere each
+		final BranchGroup viewBranch = new BranchGroup();
+		viewBranch.setCapability( BranchGroup.ALLOW_CHILDREN_WRITE );
+		
+		// init the structures needed to code the position of the beads
+		final Transform3D transform = new Transform3D();
+		final Point3f translation = new Point3f();
+		final float[] loc = new float[ 3 ];
+
+		final Random random = new Random( 1 );
+
+		// add all beads
+		for ( final Cell cell : cells.values() )
+		{
+			// set the bead coordinates
+			cell.getPosition().localize( loc );
+			translation.set( loc );
+
+			// transform the bead coordinates into the position of the view
+			globalTransform.transform( translation );
+
+			// create new TransformGroup with the altered coordinates 
+			transform.setTranslation( new Vector3f( translation ) );
+			final TransformGroup transformGroup = new TransformGroup( transform );
+
+			transformGroup.setCapability( TransformGroup.ALLOW_TRANSFORM_WRITE );
+			transformGroup.setCapability( TransformGroup.ALLOW_CHILDREN_WRITE );
+
+			// add the sphere
+			final int r = random.nextInt( 256 );
+			final int g = random.nextInt( 256 );
+			final int b = random.nextInt( 256 );
+			final Appearance appearance = new Appearance();
+			appearance.setColoringAttributes( new ColoringAttributes( new Color3f( r/255f, g/255f, b/255f ), ColoringAttributes.SHADE_GOURAUD ) );
+			appearance.setTransparencyAttributes( new TransparencyAttributes( TransparencyAttributes.NICEST, transparency ) );
+
+			appearance.getColoringAttributes().setCapability( ColoringAttributes.ALLOW_COLOR_READ );
+			appearance.getColoringAttributes().setCapability( ColoringAttributes.ALLOW_COLOR_WRITE );
+
+			final Sphere s = new Sphere( cell.getRadius(), Sphere.BODY, 10, appearance );
+			s.setCapability( Sphere.ENABLE_APPEARANCE_MODIFY );
+			s.getShape().setCapability( Shape3D.ALLOW_APPEARANCE_WRITE );
+			transformGroup.addChild( s );
+
+			// store the link between cell and sphere
+			cell.setSphere( s );
+
+			// add the group to the view branch
+			viewBranch.addChild( transformGroup );
+		}
+
+		// ????
+		viewBranch.compile();
+
+		// add the view branch to the scene
+		parent.addChild( viewBranch );
+		
+		return viewBranch;
+	}
+
+	public static Content drawInvisibleBoundingBox( final Image3DUniverse univ, final Map< Integer, Cell > cells )
+	{
+		float[] min = null;
+		float[] max = null;
+
+		for ( final Cell cell : cells.values() )
+		{
+			if ( min == null || max == null )
+			{
+				min = new float[ 3 ];
+				max = new float[ 3 ];
+
+				for ( int d = 0; d < min.length; ++d )
+					min[ d ] = max[ d ] = cell.getPosition().getFloatPosition( d );
+			}
+
+			for ( int d = 0; d < min.length; ++d )
+			{
+				min[ d ] = Math.min( min[ d ], cell.getPosition().getFloatPosition( d ) );
+				max[ d ] = Math.max( max[ d ], cell.getPosition().getFloatPosition( d ) );
+			}
+		}
+
+		final List< Point3f > mesh = new ArrayList< Point3f >();
+
+		mesh.add( new Point3f( min ) );
+		mesh.add( new Point3f( max ) );
+
+		final CustomPointMesh cm = new CustomPointMesh( mesh );
+		cm.setColor( new Color3f( 0, 1, 0 ) );
+		cm.setPointSize( 0 );
+
+		final Content content = univ.addCustomMesh( cm, "invisible bounding box" );
+		content.showCoordinateSystem( false );
+
+		return content;
+	}
+
+	public static BranchGroup drawNucleiSlow( final Image3DUniverse univ, final Map< Integer, Cell > cells, final Transform3D globalTransform, final float transparency )
+	{
+		final int meridians = 6;
+		final int parallels = 6;
+
+		final List< Point3f > meshes = new ArrayList<Point3f>();
+
+		// add all beads
+		for ( final Cell cell : cells.values() )
+		{
+			final List< Point3f > mesh = Mesh_Maker.createSphere(
+					cell.getPosition().getDoublePosition( 0 ),
+					cell.getPosition().getDoublePosition( 1 ),
+					cell.getPosition().getDoublePosition( 2 ),
+					cell.getRadius(),
+					meridians,
+					parallels );
+
+			meshes.addAll( mesh );
+		}
+
+		univ.addTriangleMesh( meshes, new Color3f( 1, 1, 1 ), "nuclei" );
+
+		return null;
+	}
+
+	protected synchronized boolean loadAnnotations()
+	{
+		if ( defaultXML != null )
+		{
+			fileChooser.setSelectedFile( new File( defaultXML ) );
+		}
+
+		final int returnVal = fileChooser.showDialog( null, "Open" );
+
+		if ( returnVal == JFileChooser.APPROVE_OPTION )
+		{
+			final File file = fileChooser.getSelectedFile();
+			defaultXML = file.getAbsolutePath();
+
+			System.out.println( "loading annotions from " + file );
+
+			cells.clear();
+			nextCellId = 0;
+
+			try
+			{
+				final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				final DocumentBuilder db = dbf.newDocumentBuilder();
+				final Document dom = db.parse( file );
+				final Element root = dom.getDocumentElement();
+				final NodeList nodes = root.getElementsByTagName( "sphere" );
+				for ( int i = 0; i < nodes.getLength(); ++i )
+				{
+					final Cell cell = Cell.fromXml( ( Element ) nodes.item( i ) );
+					cells.put( cell.getId(), cell );
+					if ( cell.getId() >= nextCellId )
+						nextCellId = cell.getId() + 1;
+				}
+
+				System.out.println( "Loaded " + cells.keySet().size() + " cells." );
+			}
+			catch ( final Exception e )
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	protected JFileChooser createFileChooser()
+	{
+		final JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter( new FileFilter()
+		{
+			@Override
+			public String getDescription() { return "xml files"; }
+
+			@Override
+			public boolean accept( final File f )
+			{
+				if ( f.isDirectory() )
+					return true;
+				if ( f.isFile() )
+				{
+					final String s = f.getName();
+					final int i = s.lastIndexOf('.');
+					if (i > 0 &&  i < s.length() - 1) {
+						final String ext = s.substring(i+1).toLowerCase();
+						return ext.equals( "xml" );
+					}
+				}
+				return false;
+			}
+		} );
+
+		return fileChooser;
+	}
+
+	public static void main( String[] args )
+	{
+		final VisualizeSegmentation vis = new VisualizeSegmentation();
+		vis.discoWorm();
+	}
+}
