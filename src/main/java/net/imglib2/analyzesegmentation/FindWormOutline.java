a601aa8d59a721aed41c16d7a67b53ee77c585c9
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-10-18 10:48:07 -0400

    Search is speeded up using KDTree Only using 1/3 of the actual vector making it more stable

--- src/main/java/net/imglib2/analyzesegmentation/FindWormOutline.java
+++ src/main/java/net/imglib2/analyzesegmentation/FindWormOutline.java
@@ -8,12 +8,15 @@ import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 
+import net.imglib2.RealPoint;
 import net.imglib2.analyzesegmentation.wormfit.FirstInlierCells;
 import net.imglib2.analyzesegmentation.wormfit.InlierCell;
 import net.imglib2.analyzesegmentation.wormfit.InlierCells;
 import net.imglib2.analyzesegmentation.wormfit.Score;
 import net.imglib2.analyzesegmentation.wormfit.ScoreVolume;
+import net.imglib2.collection.KDTree;
 import net.imglib2.multithreading.SimpleMultiThreading;
+import net.imglib2.neighborsearch.RadiusNeighborSearchOnKDTree;
 
 public class FindWormOutline
 {
@@ -22,7 +25,7 @@ public class FindWormOutline
 	final Cell cell0, cell1;
 	final float initialRadius;
 
-	protected float[] vectorStep = new float[]{ 40f, 30f, 20f, 15f, 10f, 7.5f, 5f, 2f, 1f };
+	protected float[] vectorStep = new float[]{ 20f, 15f, 10f, 5f, 2f, 1f };
 
 	public FindWormOutline( final Image3DUniverse univ, final Cells cells, final Cell cell0, final Cell cell1, final float initialRadius )
 	{
@@ -64,7 +67,12 @@ public class FindWormOutline
 				previousInliers.getP1().x - previousInliers.getP0().x,
 				previousInliers.getP1().y - previousInliers.getP0().y,
 				previousInliers.getP1().z - previousInliers.getP0().z );
-		final float refL = sv.length();
+		final float refL;
+
+		if ( FirstInlierCells.class.isInstance( previousInliers ) )
+			refL = sv.length();
+		else
+			refL = sv.length() * 3;
 
 		final Point3f sp = new Point3f( previousInliers.getP1() );
 
@@ -119,7 +127,7 @@ public class FindWormOutline
 										best.visualizeInliers( univ, cells, c );
 		
 										System.out.println( step + ": l=" + l + " r0=" + r0 + " r1=" + r1 + " score=" + score.score( previousInliers, best ) + " |cells|=" + best.getInlierCells().size() );
-										SimpleMultiThreading.threadWait( 250 );
+										SimpleMultiThreading.threadWait( 100 );
 									}
 								}
 						}
@@ -128,7 +136,29 @@ public class FindWormOutline
 			// update the search vector to the best solution from this scale
 			sv.set( bestSV );
 		}
+
+		//
+		// only take 1/3 of the vector
+		//
+		final Vector3f v = new Vector3f(
+				best.getP1().x - best.getP0().x,
+				best.getP1().y - best.getP0().y,
+				best.getP1().z - best.getP0().z );
+
+		Algebra.normalizeLength( v, v.length() / 3.0f );
+		
+		final Point3f p1 = new Point3f(
+				best.getP0().x + v.x,
+				best.getP0().y + v.y,
+				best.getP0().z + v.z );
+
+		best.unvisualizeInliers( univ, cells );
+		best = testGuess( best.getP0(), p1, best.getR0(), best.getR0() * 2.0f/3.0f + best.getR1() * 1.0f/3.0f, cells );
+		best.visualizeInliers( univ, cells, c );
+
+		SimpleMultiThreading.threadWait( 250 );
 		//SimpleMultiThreading.threadHaltUnClean();
+
 		return best;
 	}
 
@@ -139,9 +169,24 @@ public class FindWormOutline
 
 		final double r[] = new double[ 2 ];
 
+		// reference point is in the middle of the segmented line
+		final RealPoint ref = new RealPoint( new double[]{
+				(p1.x - p0.x)/2 + p0.x,
+				(p1.y - p0.y)/2 + p0.y,
+				(p1.z - p0.z)/2 + p0.z } );
+
+		// search radius is the half the length of the vector + maxR
+		final Vector3f v = new Vector3f( p1.x - p0.x, p1.y - p0.y, p1.z - p0.z );
+
+		// look for all points in range
+		RadiusNeighborSearchOnKDTree< Cell > search = cells.getSearch();
+		search.search( ref, v.length() / 2.0 + Math.max( r0, r1 ), false );
+
 		// test which points are along the defined line segment
-		for ( final Cell cell : cells.getCells().values() )
+		for ( int c = 0; c < search.numNeighbors(); ++c )
 		{
+			final Cell cell = search.getSampler( c ).get();
+
 			q.x = cell.getPosition().getFloatPosition( 0 );
 			q.y = cell.getPosition().getFloatPosition( 1 );
 			q.z = cell.getPosition().getFloatPosition( 2 );
