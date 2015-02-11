14019a1bd04db7b2e817ddc0cc949a6b05891555
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-02-11 15:12:08 -0500

    set right parameters

--- src/main/java/net/imglib2/drift/CorrectDrift.java
+++ src/main/java/net/imglib2/drift/CorrectDrift.java
@@ -2,45 +2,43 @@ package net.imglib2.drift;
 
 import mpicbg.models.AbstractModel;
 import mpicbg.models.RigidModel2D;
+import mpicbg.models.TranslationModel2D;
 import plugin.DescriptorParameters;
 
 public class CorrectDrift
 {
 
-	protected DescriptorParameters getParametersForProjection( final AbstractModel< ? > model )
+	protected DescriptorParameters getParametersForProjection()
 	{
 		final DescriptorParameters params = new DescriptorParameters();
 		
 		params.dimensionality = 2;
-		params.sigma1 = 2.99f; // before 2.099f
-		params.sigma2 = 3.55f; // before 2.4961457f;
-		params.threshold = 0.010566484f;
+		params.sigma1 = 6.9450;
+		params.sigma2 = 8.270341f;
+		params.threshold = 0.014f;
 		params.lookForMaxima = true;
 		params.lookForMinima = true;
-		
-		if ( model == null )
-			params.model = new RigidModel2D();
-		else
-			params.model = model;
-			
+
+		params.model = new TranslationModel2D();
+
 		params.similarOrientation = true;
 		params.numNeighbors = 3;
 		params.redundancy = 1;
-		params.significance = 3;
+		params.significance = 2;
 		params.ransacThreshold = 5;
-		params.channel1 = 0;
-		params.channel2 = 0;
+		params.channel1 = 1;
+		params.channel2 = 1;
 		
 		// for stack-registration
-		params.globalOpt = 0; // 0=all-to-all; 1=all-to-all-withrange; 2=all-to-1; 3=Consecutive
-		params.range = 5;	
+		params.globalOpt = 1; // 0=all-to-all; 1=all-to-all-withrange; 2=all-to-1; 3=Consecutive
+		params.range = 3;
 		params.directory = "";
 		
 		params.reApply = false;
 		params.roi1 = null;
 		params.roi2 = null;
 		
-		params.setPointsRois = true;
+		params.setPointsRois = false;
 		
 		params.silent = false;
 		
