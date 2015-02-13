25d4860db18fb6a27969c4adbbef79ef3f4a43b4
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-02-13 14:21:52 -0500

    fitting linear functions

--- src/main/java/net/imglib2/drift/CorrectDrift.java
+++ src/main/java/net/imglib2/drift/CorrectDrift.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import net.imglib2.drift.fit.Line;
+import net.imglib2.drift.fit.PointFunctionMatch;
 import ij.CompositeImage;
 import ij.ImageJ;
 import ij.ImagePlus;
 import mpicbg.models.InvertibleBoundable;
+import mpicbg.models.Point;
 import mpicbg.models.TranslationModel2D;
 import mpicbg.spim.io.TextFileAccess;
 import plugin.DescriptorParameters;
@@ -26,15 +29,63 @@ public class CorrectDrift
 		if ( !driftFile.exists() )
 			computeIndividualDrifts( dir, file, numChannels );
 
-		final ArrayList< float[] > drifts = loadDrifts( driftFile );
+		final float[] driftsX = loadDrifts( driftFile, 1 );
+		final float[] driftsY = loadDrifts( driftFile, 1 );
 
-		for ( final float[] drift : drifts )
-			System.out.println( drift[ 0 ] + "\t" + drift[ 1 ] );
+		removeOutliers( driftsX, 10, 0.5 );
+		removeOutliers( driftsY, 10, 0.5 );
+
+		//for ( final float[] drift : drifts )
+		//	System.out.println( drift[ 0 ] + "\t" + drift[ 1 ] );
 	}
 
-	protected ArrayList< float[] > loadDrifts( final File driftFile )
+	
+	public static int removeOutliers( final float[] drift, final double epsilon, final double minInlierRatio )
 	{
-		final ArrayList< float[] > drifts = new ArrayList< float[] >();
+		final ArrayList< PointFunctionMatch > candidates = new ArrayList<PointFunctionMatch>();
+		final ArrayList< PointFunctionMatch > inliers = new ArrayList<PointFunctionMatch>();
+
+		for ( int z = 0; z < drift.length; ++ z )
+			if ( drift[ z ] != 0 )
+				candidates.add( new PointFunctionMatch( new Point( new float[]{ z, drift[ z ] } ) ) );
+
+		int numRemoved = 0;
+
+		try
+		{
+			final Line l = new Line();
+
+			l.ransac( candidates, inliers, 1000, epsilon, minInlierRatio );
+			numRemoved = candidates.size() - inliers.size();
+
+			l.fit( inliers );
+
+			System.out.println( "y = " + l.getM() + " x + " + l.getN() + ", " + numRemoved + " points removed." );
+
+			/*
+			offsets.clear();
+			
+			for ( final PointFunctionMatch p : inliers )
+			{
+				final LinkedPoint< ? > lp = (LinkedPoint< ? >)p.getP1();
+				offsets.add( (PlaneOffset)lp.getLinkedObject() );
+			}
+			*/
+			//for ( final PointFunctionMatch p : inliers )
+			//	System.out.println( l.distanceTo( p.getP1() ) );
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			numRemoved = drift.length;
+		}
+		return numRemoved;
+	}
+
+	protected float[] loadDrifts( final File driftFile, final int column )
+	{
+		final ArrayList< Float > drifts = new ArrayList< Float >();
 		final BufferedReader in = TextFileAccess.openFileRead( driftFile );
 
 		try
@@ -42,7 +93,7 @@ public class CorrectDrift
 			while ( in.ready() )
 			{
 				String[] values = in.readLine().trim().split( "\t" );
-				drifts.add( new float[]{ Float.parseFloat( values[ 1 ] ), Float.parseFloat( values[ 2 ] ) } );
+				drifts.add( Float.parseFloat( values[ column ] ) );
 			}
 		}
 		catch (IOException e)
@@ -51,7 +102,11 @@ public class CorrectDrift
 			return null;
 		}
 
-		return drifts;
+		final float[] d = new float[ drifts.size() ];
+		for ( int i = 0; i < d.length; ++i )
+			d[ i ] = drifts.get( i );
+
+		return d;
 	}
 
 	protected void computeIndividualDrifts( final String dir, final String file, final int numChannels )
@@ -148,8 +203,8 @@ public class CorrectDrift
 		new ImageJ();
 
 		final int numChannels = 2;
-		String dir = "/media/preibisch/data/Microscopy/confocal/TestAcquisitions";
-		//String  dir = "/Volumes/My Passport/confocal";
+		//String dir = "/media/preibisch/data/Microscopy/confocal/TestAcquisitions";
+		String  dir = "/Volumes/My Passport/confocal";
 
 		for ( int i = 1; i <= 4; ++i )
 			new CorrectDrift( dir, "worm" + i + ".zip", numChannels );
